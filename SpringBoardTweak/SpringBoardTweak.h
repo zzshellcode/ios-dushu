22ea4c8d2d2b36640c6ddf79b96e3903abd4d5be
zzshellcode <zz638614@gmail.com>
2026-07-23 21:49:24 +0800

    Update SpringBoardTweak.h

--- SpringBoardTweak/SpringBoardTweak.h
+++ SpringBoardTweak/SpringBoardTweak.h
@@ -1,8 +1,1 @@
-@import UIKit;
 
-static void showAlert(NSString *title, NSString *message);
-
-@interface SpringBoard : UIApplication
-+ (SpringBoard *)sharedApplication;
-- (UIView *)statusBarForEmbeddedDisplay;
-@end
